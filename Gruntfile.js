28a60bd828a1282ded1169d0233421a6b7d434eb
wangying1998 <1610010554@qq.com>
2019-03-12 16:31:51 +0800

    eslint

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: './.eslintrc.json'
+      },
+      target: ['./rectangle.js']
+    }
+  });
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.registerTask('default', ['eslint']);
+};
