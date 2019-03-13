30044921ed34f9a60ef2381953c357d737d7dd40
wangying1998 <1610010554@qq.com>
2019-03-13 15:24:26 +0800

    http-api-test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,22 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    run: {
+      api: {
+          options: { wait: false  },
+          args: ['./app.js']
+      }               
+    },
     mochacli: {
       options: {
-          reporter: 'spec',
-          bail: true
+        reporter: 'spec',
+        bail: true         
       },
-      all: ['test/rectangle.test.js']                         
-    },
+      all: ['test/*.js']                          
+    }  
   });
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.registerTask('default', ['mochacli']);   
-}
+
+  grunt.loadNpmTasks('grunt-run')
+  grunt.loadNpmTasks('grunt-mocha-cli')
+  grunt.registerTask('default', ['run', 'mochacli', 'stop:api']);
+
+};
--- test/weather.test.js
+++ test/weather.test.js
@@ -1,5 +1,17 @@
 const expect = require('chai').expect;
+describe('天气接口测试',function(){
+  it('石家庄的天气',function(){
+    const http = require('http');
+    http.get('http://v.juhe.cn/weather/index?cityname=石家庄&key=f4de9e662b38edb1a376be17145f680f',(res)=>{
+      var reault = '';
+      res.on('data',(chunk)=>{
+        result+=chunk;
+      });
+      res.on('end',()=>{
+        console.log(JSON.parse(result));
+      })
 
-expect.
-
+    })
+  })
+})
 
